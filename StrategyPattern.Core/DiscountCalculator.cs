2e61b4cc33822418622c50a004388f28d2ddcc96
Aniss <aniss.mahfoudi@gmail.com>
2025-01-07 15:11:16 +0100

    Startegy Pattern

---
+++ StrategyPattern.Core/DiscountCalculator.cs
@@ -0,0 +1,28 @@
+﻿using DiscountLibrary.Interfaces;
+
+namespace DiscountLibrary
+{
+    public class DiscountCalculator
+    {
+        // Reference vers la stratégie actuelle
+        private IDiscountStrategy _strategy;
+
+
+        public DiscountCalculator(IDiscountStrategy strategy)
+        {
+            _strategy = strategy;
+        }
+
+        // Permet de changer de stratégie
+        public void SetDiscountStrategy(IDiscountStrategy strategy)
+        {
+            _strategy = strategy;
+        }
+
+        // Déclenche l'éxécution de la stratégie
+        public decimal Calculate(decimal total)
+        {
+            return _strategy.CalculateDiscount(total);
+        }
+    }
+}
---
+++ StrategyPattern.Core/DiscountStrategies/ThresholdDiscountStrategy.cs
@@ -0,0 +1,23 @@
+﻿
+using DiscountLibrary.Interfaces;
+
+namespace DiscountLibrary.DiscountStrategies
+{
+    internal class ThresholdDiscountStrategy : IDiscountStrategy
+    {
+        private readonly decimal _threshold;
+        private readonly decimal _flatDiscount;
+
+        public ThresholdDiscountStrategy(decimal threshold, decimal flatDiscount)
+        {
+            _threshold = threshold;
+            _flatDiscount = flatDiscount;
+        }
+
+        public decimal CalculateDiscount(decimal total)
+        {
+            return total >= _threshold ? total - _flatDiscount : total;
+        }
+    }
+
+}
